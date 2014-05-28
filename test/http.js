34ea64f0d6b303a697886e75f2263c7274b53d90
Brandon Valosek <bvalosek@gmail.com>
2014-05-27 22:51:40 -0500

    change order of mocks

--- test/http.js
+++ test/http.js
@@ -6,23 +6,25 @@ test('configs are set / read', function(t) {
   t.plan(2);
 
   // All the config stuff
-  var config = mock
-        .mock('get').takes('http.port', process.env.PORT || 8123);
-  config.mock('get').takes('http.server', null);
-  config.mock('get').takes('http.secret', 'supersecret');
+  var config = mock.mock();
+  var app    = mock.mock();
 
   // Registers are stuff
-  var app = mock.mock('register')
+  app.mock('register')
     .takesF(function(tag, thing) { return tag === 'http'; })
     .returns(mock.mock('asInstance').takesAll());
 
-  // Fire
   var server = new HttpServer(app, config);
 
   // fake out express with a successful connnect
   server.http = mock.mock('listen').takesF(function(port, callback) {
     callback(null);
   });
+
+  // Add in all mocks here to ensure happens after init
+  config.mock('get').takes('http.port', process.env.PORT || 8123);
+  config.mock('get').takes('http.server', null);
+  config.mock('get').takes('http.secret', 'supersecret');
 
   server.start().then(function() {
     t.pass('made it');
